4d58f21791ee81aeba74b337226bbb7c26853357
Jonathan Castillo <jonathan@codeforaustralia.org>
2018-10-31 18:31:34 +1100

    Update

--- code/web/src/services/coreApiGraphql.js
+++ code/web/src/services/coreApiGraphql.js
@@ -1,12 +1,9 @@
 const axios = require("axios");
-
 export default class coreApiGraphql {   
 
-    constructor() {
-             
+    constructor() {             
       }
       async postData(query) {
-
         return axios({
              url: 'http://localhost:4000/graphql',
              method: 'post',
@@ -17,42 +14,36 @@ export default class coreApiGraphql {
                  /* eslint-disable */
                  console.log('graphql error:', err);                
              });
-     }
-
-    createBusiness(model) {
-      // Use vue-resource or any other http library to send your request
-      var query=` mutation{
-                createBusiness(input:{id:"${model.id}",abn:"${model.abn}",name:"${model.name}"}){
-                    id                                            
-                    abn
-                    name
-                }
-            }`;        
-        /* eslint-disable */
-        return this.postData(query).then((business) => {
-                this.createPerson(model).then((person)=>{                     
-                        this.createGraphEdge({  type:"business_person",
-                                                a:business.createBusiness.id,
-                                                b:person.createPerson.id
-                                              })
-
-                })
-            }); 
     }
-    createPerson(model) {
-        // Use vue-resource or any other http library to send your request
+    createBusiness(model) {      
+        var query=` mutation{
+            createBusiness(input:{id:"${model.id}",abn:"${model.abn}",name:"${model.name}"}){
+                id                                            
+                abn
+                name
+            }
+        }`;   
+        return this.postData(query).then((business) => {
+            this.createPerson(model).then((person)=>{                     
+                    this.createGraphEdge({  type:"business_person",
+                                            a:business.createBusiness.id,
+                                            b:person.createPerson.id
+                                            })
+
+            })
+        }); 
+    }
+    createPerson(model) {        
         var query=` mutation{
                   createPerson(input:{email:"${model.email}",name:"${model.name}"}){
                       id                                            
                       email
                       name
                   }
-              }`;          
-          /* eslint-disable */
+              }`;  
          return this.postData(query);  
       }  
-      createGraphEdge(obj){
-        
+    createGraphEdge(obj){    
         // createGraphEdge(input:${JSON.stringify(obj)})
         var query=` mutation{
             createGraphEdge(input:{type:"${obj.type}",a:"${obj.a}",b:"${obj.b}"})
@@ -62,8 +53,7 @@ export default class coreApiGraphql {
                                 a
                                 b
                             } 
-        }`;        
-        
+        }`;                
         return this.postData(query); 
-      }
+    }
  }
