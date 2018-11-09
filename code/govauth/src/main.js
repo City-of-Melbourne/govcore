4db5566b2ce13f74cd28bee128fcf421a942173e
Victor Nava <victornavav@gmail.com>
2018-11-09 15:52:13 +1100

    Add user profile

--- code/govauth/src/main.js
+++ code/govauth/src/main.js
@@ -4,25 +4,31 @@ import App from './App.vue';
 import router from "./router";
 import Default from "./layouts/Default.vue";
 import NoSidebar from "./layouts/NoSidebar.vue";
+import coreApiGraphql from './services/coreApiGraphql';
+
 
 
 import './assets/scss/ga.scss'
 Vue.config.productionTip = false
 Vue.component("default-layout", Default);
 Vue.component("no-sidebar-layout", NoSidebar);
+
 Vue.use(Buefy)
 
+// TODO replace with logged user
+const person = {
+    name: "V",
+    email: "v@cfa.org",
+    bucket: "entities",
+    type: "person",
+    id: "3786048077"
+}
 
 
-
-new Vue({  
+new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  data: {
+      person: person
+  }
 }).$mount('#app')
-
-
-
-
-
-
-
