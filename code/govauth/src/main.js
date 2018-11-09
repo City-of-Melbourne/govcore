bd2e867ffab50a1a5271a3e8639eaea5b8010efa
Jonathan Castillo <jonathan@codeforaustralia.org>
2018-11-09 15:56:30 +1100

    Solving Conflict

--- code/govauth/src/main.js
+++ code/govauth/src/main.js
@@ -4,21 +4,15 @@ import App from './App.vue';
 import router from "./router";
 import Default from "./layouts/Default.vue";
 import NoSidebar from "./layouts/NoSidebar.vue";
-<<<<<<< HEAD
-=======
 import coreApiGraphql from './services/coreApiGraphql';
 
 
 
->>>>>>> 4db5566b2ce13f74cd28bee128fcf421a942173e
 import './assets/scss/ga.scss'
 Vue.config.productionTip = false
 Vue.component("default-layout", Default);
 Vue.component("no-sidebar-layout", NoSidebar);
 
-<<<<<<< HEAD
-new Vue({  
-=======
 Vue.use(Buefy)
 
 // TODO replace with logged user
@@ -32,7 +26,6 @@ const person = {
 
 
 new Vue({
->>>>>>> 4db5566b2ce13f74cd28bee128fcf421a942173e
   router,
   render: h => h(App),
   data: {
